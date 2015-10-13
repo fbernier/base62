6f07aa9fe42b6c4cf4a896330b15c86df2884d4c
Francois Bernier <frankbernier@gmail.com>
2015-10-13 09:26:13 -0400

    minor improvements

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 pub mod base62 {
-    const BASE: i32 = 62;
-    const ALPHABET: [char; BASE as usize] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-                                               'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
-                                               'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-                                               'v', 'w', 'x', 'y', 'z'];
+    const BASE: u64 = 62;
+    const ALPHABET: [char; BASE as usize] =
+        ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+        'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+        'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+        'v', 'w', 'x', 'y', 'z'];
 
-    pub fn encode(mut num: i32) -> String {
-        let mut result: String = "".to_string();
+    pub fn encode(mut num: u64) -> String {
+        if num == 0 { return "0".to_string() }
+        let mut result  = String::new();
 
         while num > 0 {
-            let char_holder = ALPHABET.get((num % BASE) as usize).unwrap().to_string();
+            let char_holder = ALPHABET[(num % BASE) as usize].to_string();
             result = char_holder + &result;
             num /= BASE
         }
@@ -17,12 +19,12 @@ pub mod base62 {
         return result.to_string();
     }
 
-    pub fn decode(string: &str) -> i32 {
+    pub fn decode(string: &str) -> u64 {
         let mut result = 0;
 
         for (i, c) in string.chars().rev().enumerate() {
             let num = BASE.pow(i as u32);
-            result += ALPHABET.iter().position(|x| *x == c).unwrap() as i32 * num;
+            result += ALPHABET.iter().position(|x| *x == c).unwrap() as u64 * num;
         }
 
         return result;
