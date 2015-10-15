46e041b0f1bfca9354cb93418141669aab120493
Francois Bernier <frankbernier@gmail.com>
2015-10-15 19:39:37 -0400

    Use bytes instead of chars Return a Result for decode

---
+++ src/base62.rs
@@ -0,0 +1,75 @@
+use std::fmt;
+pub use self::DecodeError::*;
+
+const BASE: u64 = 62;
+const ALPHABET: [u8; BASE as usize] =
+[b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I',
+b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z', b'a', b'b',
+b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u',
+b'v', b'w', b'x', b'y', b'z'];
+
+pub fn encode(mut num: u64) -> String {
+    if num == 0 { return "0".to_owned() }
+    let mut bytes = Vec::new();
+
+    while num > 0 {
+        bytes.push(ALPHABET[(num % BASE) as usize]);
+        num /= BASE
+    }
+    bytes.reverse();
+
+    String::from_utf8(bytes).unwrap()
+}
+
+pub enum DecodeError { InvalidBase62Byte(char, usize) }
+
+impl fmt::Debug for DecodeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            InvalidBase62Byte(ch, idx) =>
+                write!(f, "Invalid character '{}' at position {}", ch, idx)
+        }
+    }
+}
+
+impl fmt::Display for DecodeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(&self, f)
+    }
+}
+
+pub fn decode(string: &str) -> Result<u64, DecodeError> {
+    let mut result = 0;
+
+    for (i, c) in string.as_bytes().iter().rev().enumerate() {
+        let num = BASE.pow(i as u32);
+        match ALPHABET.binary_search(&c) {
+            Ok(v) => { result += v as u64 * num },
+            Err(_e) => {
+                return Err(InvalidBase62Byte(c.to_owned() as char, string.len() - i))
+            }
+        }
+    }
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use base62;
+
+    #[test]
+    fn test_encode() {
+        assert_eq!(base62::encode(1337), "LZ");
+    }
+
+    #[test]
+    fn test_decode() {
+        assert_eq!(base62::decode("LZ").unwrap(), 1337);
+    }
+
+    #[test]
+    fn test_decode_invalid_char() {
+        assert!(base62::decode("{").is_err());
+    }
+}
